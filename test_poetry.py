0d4fc183a26d9abd8c7415e82def34fad8bd69f9
高俊奇 <junqi@teambition.com>
2017-05-26 09:51:30 +0800

    [UPDATE] 修复单元测试BUG

--- test_poetry.py
+++ test_poetry.py
@@ -7,14 +7,14 @@
 
 import os, json
 
-def check_json(json):
-    filepath = os.path.join('./json', json)
+def check_json(f):
+    filepath = os.path.join('./json', f)
     with open(filepath) as file:
         try:
             _ = json.loads(file.read())
             return True
         except:
-            assert False, "校验(%s)失败" % json
+            assert False, "校验(%s)失败" % f
 
 
 def test_json():
