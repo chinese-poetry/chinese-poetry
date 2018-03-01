9efc28fa932ac7052b06bd4bb74376b65f1aa8ba
jackeyGao <gaojunqi@outlook.com>
2018-03-01 13:32:42 +0800

    test: 增加词内容 JSON 文件校验，去除 ci.db 文件校验

--- test_poetry.py
+++ test_poetry.py
@@ -1,8 +1,11 @@
 #! -*- coding: utf-8 -*-
 import os, json, sqlite3
 
-def check_json(f):
-    filepath = os.path.join('./json', f)
+def check_json(f, _dir):
+    if not f.endswith('.json'):
+        return True
+
+    filepath = os.path.join(_dir, f)
     with open(filepath) as file:
         try:
             _ = json.loads(file.read())
@@ -11,24 +14,39 @@ def check_json(f):
             assert False, u"校验(%s)失败" % f
 
 
-def test_json():
+def test_shi_json():
     """
         测试古诗JSON文件是否有效
     """
-    map(check_json, os.listdir('./json'))
+    [ check_json(f, './json') for f in os.listdir('./json') ]
 
 
-
-def test_sqlite():
+def test_ci_json():
     """
-        测试ci数据库文件是否有效
+        测试词JSON文件是否有效
     """
-    conn = sqlite3.connect('./ci/ci.db')
+    [ check_json(f, './ci') for f in os.listdir('./ci') ]
 
-    c = conn.cursor()
 
-    c.execute("SELECT name FROM sqlite_master WHERE type='table'")
+#def test_sqlite():
+#    """
+#        测试ci数据库文件是否有效
+#    """
+#    conn = sqlite3.connect('./ci/ci.db')
+#
+#    c = conn.cursor()
+#
+#    c.execute("SELECT name FROM sqlite_master WHERE type='table'")
+#
+#    tables = c.fetchall()
+#    
+#    assert len(tables) == 2, u"Sqlite文件异常"
 
-    tables = c.fetchall()
-    
-    assert len(tables) == 2, u"Sqlite文件异常"
+
+def main():
+    test_shi_json()
+    test_ci_json()
+
+
+if __name__ == '__main__':
+    main()
