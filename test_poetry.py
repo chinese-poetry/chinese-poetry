405f6baceb2ce04f52baa33ceabc7b91ece18a0e
jackeyGao <gaojunqi@outlook.com>
2018-12-21 15:51:53 +0800

    doc: add contributors svg

--- test_poetry.py
+++ test_poetry.py
@@ -37,3 +37,5 @@ test_lunyu = functools.partial(__check_path__, './lunyu')
 test_huajianji = functools.partial(__check_path__, u'./wudai/花间集/')
 
 test_nantang2 = functools.partial(__check_path__, u'./wudai/南唐二主词/')
+
+test_nantang2 = functools.partial(__check_path__, u'./youmengying/')
