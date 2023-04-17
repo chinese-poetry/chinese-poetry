fa1814a8044db4fb0c7a9e0a0449974e8c3463b7
JackeyGao <gaojunqi@outlook.com>
2023-04-17 14:13:23 +0800

    修改为中文目录

--- test_poetry.py
+++ test_poetry.py
@@ -5,46 +5,43 @@ import sys
 import traceback
 import functools
 
+namespace = locals()
+
 
 def check_json(f, _dir):
+    filepath = os.path.join(_dir, f)
+
+    if os.path.isdir(filepath):
+        [check_json(f, filepath) for f in os.listdir(filepath)]
+
     if not f.endswith('.json'):
         return True
 
-    filepath = os.path.join(_dir, f)
     with open(filepath) as file:
         try:
             _ = json.loads(file.read())
             sys.stdout.write(f"{filepath} 校验成功")
             return True
-        except:
+        except Exception as e:
             sys.stderr.write(traceback.format_exc())
-            assert False, f"{filepath} 校验失败"
+            print(dir(e))
+            assert False, f"{filepath} 校验失败, {e}"
 
 
-def __check_path__(path):
+def check_path(path):
     """校验 指定目录 中的 json 文件"""
-    [ check_json(f, path) for f in os.listdir(path) ]
+    [check_json(f, path) for f in os.listdir(path)]
 
 
-test_shi = functools.partial(__check_path__, './json')
+def is_book_directory(book):
+    if not os.path.isdir(book):
+        return False
 
-test_ci = functools.partial(__check_path__, './ci')
+    for i in book:
+        if i > u'\u4e00' and i < u'\u9fff':
+            return True
+    return False
 
-test_shijing = functools.partial(__check_path__, './shijing')
 
-test_lunyu = functools.partial(__check_path__, './lunyu')
-
-test_huajianji = functools.partial(__check_path__, u'./wudai/huajianji/')
-
-test_nantang2 = functools.partial(__check_path__, u'./wudai/nantang/')
-
-test_youmengying = functools.partial(__check_path__, u'./youmengying/')
-
-test_sishuwujing = functools.partial(__check_path__, u'./sishuwujing/')
-
-test_yuanqu = functools.partial(__check_path__, u'./yuanqu/')
-
-test_mengxue = functools.partial(__check_path__, u'./mengxue')
-
-caocaoshiji = functools.partial(__check_path__, u'./caocaoshiji')
-
+for path in [i for i in os.listdir('.') if is_book_directory(i)]:
+    namespace[f'test_{path}'] = functools.partial(check_path, f'{path}')
